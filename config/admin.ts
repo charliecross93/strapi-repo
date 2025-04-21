64d054a0b68abcd40afa0844ec471afec5b8838a
Charlie Cross <fartooraw@gmail.com>
2025-04-21 19:41:29 +0100

    configure proxy + publicPath for /cms routing

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,22 @@
+// config/admin.ts
 export default ({ env }) => ({
   auth: {
     secret: env('STRAPI_ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'CjS8IQNp8acHGIWR5RU6Xw=='),
+    salt: env('API_TOKEN_SALT'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', 'defaultTransferSalt'),
+      salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  },
+
+  build: {
+    publicPath: '/cms/',
   },
 });
--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,10 @@
+// config/server.ts
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('STRAPI_PUBLIC_URL', 'https://staging.fishplanner.com'),
+  proxy: true,
   app: {
-    // Using default keys if APP_KEYS is not set in the environment.
-    keys: env.array('APP_KEYS', ['mySecretKey1', 'mySecretKey2']),
+    keys: env.array('APP_KEYS', ['mySecretKey1','mySecretKey2']),
   },
 });
