746c02bdec2bd248e3a9b993de4fa6683eb5de7a
EC2 Default User <ec2-user@ip-172-31-18-62.eu-west-2.compute.internal>
2025-04-21 18:43:59 +0000

    chore: save manual config edits before pulling

--- config/admin.ts
+++ config/admin.ts
@@ -1,16 +1,23 @@
 // config/admin.ts
 export default ({ env }) => ({
+  // mount the admin panel at /cms instead of /admin
+  serveAdminPanel: true,
+  path: '/cms',
+
   auth: {
     secret: env('STRAPI_ADMIN_JWT_SECRET'),
   },
+
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
+
   transfer: {
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
+
   flags: {
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
